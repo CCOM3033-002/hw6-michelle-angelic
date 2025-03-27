745728056fcedcf9ed7f98119e1a315ad1a14121
michelle-angelic <michelle.santiago19@upr.edu>
2025-03-27 10:41:05 +0000

    modifiying menu

--- main.cpp
+++ main.cpp
@@ -18,18 +18,19 @@ int main() {
     
     cout << "Escoja una figura: \n";
     cout << " a. Cilindro\n b. Esfera\n c. Prisma rectangular\n";
+    cout << "Selección: ";
     cin >> first_choice;
     
     switch (first_choice)
     {
         case 'a':
-        case 'A': cout << "Selección: A\n";
+        case 'A': cout << "Escogió el cilindro.\n";
                 break;
         case 'b':
-        case 'B': cout << "Selección: B\n";
+        case 'B': cout << "Escogió la esfera.\n";
                 break;
         case 'c':
-        case 'C': cout << "Selección: C\n";
+        case 'C': cout << "Escogió el prisma rectangular.\n";
                 break;
         default: cout << "No entraste A, B, o C!\n";
     }
@@ -40,15 +41,16 @@ int main() {
     
     cout << "Escoja entre los siguientes opciones: \n";
     cout << " a. Volumen\n b. Área de la superficie\n";
+    cout << "Selección: ";
     cin >> sec_choice;
     
     switch (sec_choice) {
         
         case 'a':
-        case 'A': cout << "Selección: A\n";
+        case 'A': cout << "Escogió calcular el volumen.\n";
                 break;
         case 'b':
-        case 'B': cout << "Selección: B\n";
+        case 'B': cout << "Escogió calcular el área de la superficie.\n";
                 break;
                 
         default: "No entraste A o B!\n";
